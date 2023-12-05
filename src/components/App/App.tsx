2a2d20bc256bec8e86863e9857b36711bd7576ee
zldlc <sergo.lyutskan@gmail.com>
2023-12-06 02:49:34 +0300

    added Rated page, added rate stars, change error message

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -8,6 +8,7 @@ import { Offline } from 'react-detect-offline';
 import './App.scss';
 
 const App: FC = () => {
+
   return (
     <main className="main">
       <PagesTabs />
@@ -24,5 +25,4 @@ const App: FC = () => {
 
 export default App;
 
-// +Разнеси по файлам стилевой провайдер - +создать две страницы - +изменить текст ошибки без впн - добавить UI (звездочки)
-// Поменять ховер у pagination - сверстать круг для рейтинга - сделать уменьшение шрифта больших заголовков
+
